73cb97dfd7763a7c913a0327c336b8d242948d77
arunaravindakshan <arunpriv@hotmail.com>
2025-08-14 22:11:07 +0530

    aws_config

--- 12.AWS_IAM_USER/main.tf
+++ 12.AWS_IAM_USER/main.tf
@@ -1,17 +1,7 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket"
-}
+resource "aws_iam_user" "iamuser_yousuf" {
+  name = "iamuser_yousuf"
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+  tags = {
+    tag-key = "tag-value"
   }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
 }
