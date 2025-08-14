81239c17602c63decd50f97e166dfd010d62e6e9
arunaravindakshan <arunpriv@hotmail.com>
2025-08-14 12:17:15 +0530

    Indexed

---
+++ 03.VPC_IPV6_CIDR/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "nautilus" {
+  cidr_block                     = "10.0.0.0/16"
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_support            = true
+  enable_dns_hostnames          = true
+
+  tags = {
+    Name = "nautilus-vpc"
+  }
+}
